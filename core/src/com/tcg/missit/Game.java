5ccc70e48332a9ea75c24e32cb11a7cf7f13f730
Jose Rivas <JoseRivas823@gmail.com>
2014-12-29 16:45:39 -0600

    Added GameStateManager to Game class

--- core/src/com/tcg/missit/Game.java
+++ core/src/com/tcg/missit/Game.java
@@ -23,9 +23,19 @@ public class Game extends ApplicationAdapter {
 	@Override
 	public void create () {
 		
+		int width = Gdx.graphics.getWidth();
+		int height = Gdx.graphics.getHeight();
+		SIZE = new Vector2();
+		CENTER = new Vector2();
+		
+		SIZE.set(width, height);
+		CENTER.set(width * .5f, height * .5f);
+		
 		time = 0;
 		fps = 0;
 		frames = 0;
+		
+		gsm = new GameStateManager();
 		
 		Gdx.input.setInputProcessor(new MyInputProcessor());
 		Controllers.addListener(new MyControllerProcessor());
@@ -46,10 +56,26 @@ public class Game extends ApplicationAdapter {
 		}
 		Gdx.graphics.setTitle("Miss It! | " + fps + " fps");
 		
+		gsm.handleInput();
+		gsm.update(dt);
+		gsm.draw();
+		
 		if(MyInput.isPressed(MyInput.ESCAPE)) {
 			Gdx.app.exit();
 		}
 		
 		MyInput.update();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		SIZE.set(width, height);
+		CENTER.set(width * .5f, height * .5f);
+		gsm.resize(Game.SIZE);
+	}
+
+	@Override
+	public void dispose() {
+		gsm.dispose();
 	}
 }
