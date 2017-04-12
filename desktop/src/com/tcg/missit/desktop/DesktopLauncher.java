8bd6ae8d473d7ea741683be04751d6995c4ecf30
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2017-04-12 09:34:02 -0400

    New Project Creation

---
+++ desktop/src/com/tcg/missit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tcg.missit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tcg.missit.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
